1cb4f84f3b8854768f507eb11d9798a1dc46c250
Bonam24 <bosene@usiu.ac.ke>
2024-12-12 00:53:59 +0300

    My files

---
+++ src/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+const path = require('path');
+
+
+app.set('views', path.join(__dirname, '../views'));
+app.set('view engine', 'hbs');
+app.use(express.static('public'));
+
+const homeRouter = require('../routes/homepage');
+app.use('/', homeRouter);
+
+  app.listen(port, () => {
+    console.log(`app listening at http://localhost:${port}`);
+  });
